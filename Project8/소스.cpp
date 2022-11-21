9bed6ef788138ccdafd9439b99be1b03609827a7
belstead <lim6592@naver.com>
2022-11-21 18:09:41 +0900

    upload

---
+++ Project8/소스.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+//네임스페이스의 모든 식별자들을 현재 코드 범위에 허용하는 것
+using namespace std;
+
+int main()
+{
+
+    int n = 4;
+    //행과 열 초기화
+    int column = 1, row = 1;
+    //단위행렬 만들기
+    while (column <= n)
+    {
+        row = 1;
+        while (row <= n)
+        {
+            cout << (row == column ? 1 : 0);
+            row++;
+        }
+        cout << endl;
+        column += 1;
+        continue;
+    }
+    return 0;
+}
